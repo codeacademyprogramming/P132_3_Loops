05f3c014e9842003684f30f064d950e7b0b22462
EAgha <41126467+EAghayev@users.noreply.github.com>
2022-08-08 12:53:31 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _08082022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            //int i = 1;
+
+            //while (i < 10)
+            //{
+            //    sum += i;
+            //    i++;
+            //}
+
+            for(int i = 1; i < 10; i++)
+            {
+                sum += i;
+            }
+          
+            Console.WriteLine(sum);
+
+            Console.WriteLine("===================");
+            int n = 71;
+            int m = 76;
+
+
+            bool hasNumberDividedBy7 = false;
+            while (n<m)
+            {
+                if (n % 7 == 0)
+                {
+                    hasNumberDividedBy7 = true;
+                    break;
+                }
+
+                n++;
+            }
+
+
+            if (hasNumberDividedBy7==true)
+            {
+                Console.WriteLine("Var");
+            }
+            else
+            {
+                Console.WriteLine("Yoxdur");
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
